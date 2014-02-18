98cd816823b5d29442fed9473d13a039fc79236e
Etienne Millon <me@emillon.org>
2014-02-18 08:30:34 +0100

    generic test runner

--- tests.ml
+++ tests.ml
@@ -1,20 +1,27 @@
-let tests () =
-    let res = Hashtbl.create 0 in
-    let r01 =
-        let open Bulletml in
+let testspecs =
+    let open Bulletml in
+    [ ("01.xml", `Bullet (
         Bullet (Some 270, Some 2, [
             Direct [Accel (None, Some 3, 120)]
-        ])
+            ]
+        )))
+    ]
+
+let tests () =
+    let mk_test (n, s) =
+        let run_test () =
+            let fname = "examples/" ^ n in
+            let x = Xml.parse_file fname in
+            let str = Xml.to_string_fmt x in
+            print_endline str;
+            match s with
+              | `Bullet bspec ->
+                    let b = Parser.parse_bullet x in
+                    OUnit.assert_equal b bspec
+        in
+        (n, `Quick, run_test)
     in
-    Hashtbl.add res "01.xml" r01;
-    let ex01 () =
-        let x = Xml.parse_file "examples/01.xml" in
-        let s = Xml.to_string_fmt x in
-        print_endline s;
-        let b = Parser.parse_bullet x in
-        OUnit.assert_equal (Hashtbl.find res "01.xml") b
-    in
-    [ ("01.xml", `Quick, ex01) ]
+    List.map mk_test testspecs
 
 let _ =
     Alcotest.run "BulletML" ["examples", tests ()]
