bc1f52ecc53206b0bd20f86a22c07892917ef530
Etienne Millon <me@emillon.org>
2014-02-20 20:33:43 +0100

    next_state

--- interp.ml
+++ interp.ml
@@ -8,17 +8,21 @@ let (+:) (xa, ya) (xb, yb) =
 let int_pos (x, y) =
   (int_of_float x, int_of_float y)
 
-let interp_elem = function
-  | _ -> assert false
+type state = { frame : int }
 
-let interpret (Bulletml.BulletML (hv, es)) =
-  List.iter interp_elem es
+let initial_state =
+  { frame = 0
+  }
 
-let draw_frame (window:OcsfmlGraphics.render_window) i =
+let next_state s =
+  { frame = s.frame + 1
+  }
+
+let draw_frame (window:OcsfmlGraphics.render_window) state =
   let open OcsfmlGraphics in
   let background = new rectangle_shape () in
   window#draw background;
-  let t = from_deg (10.0 *. float i) in
+  let t = from_deg (10.0 *. float state.frame) in
   let sz = 50.0 in
   let delta = (sz *. cos t, sz *. sin t) in
   let center = (100., 100.) in
@@ -30,17 +34,26 @@ let draw_frame (window:OcsfmlGraphics.render_window) i =
 let _ =
   let open OcsfmlWindow in
   let open OcsfmlGraphics in
+  let (fname, patname) = match Sys.argv with
+    | [| _ ; a1 ; a2 |] -> (a1, a2)
+    | _ -> failwith "usage: bulletml pattern.xml name"
+  in
+  let x = Xml.parse_file fname in
+  let bml = Parser.parse_xml x in
   let mode = VideoMode.create ~w:200 ~h:200 () in
   let window = new render_window mode "BulletML" in
   window#set_framerate_limit 60;
   window#set_vertical_sync_enabled true;
+  let state = ref initial_state in
   let nframe = ref 0 in
   while true; do
     if window#is_open
     then begin
+      let s = !state in
       window#clear ();
-      draw_frame window (!nframe);
+      draw_frame window s;
       window#display;
+      state := next_state s
     end;
     incr nframe
   done
