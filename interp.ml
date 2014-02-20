b8797781fda588fc33305bcf87a6b96d21ed9c7c
Etienne Millon <me@emillon.org>
2014-02-20 19:28:13 +0100

    use ocsfml instead of graphics

--- interp.ml
+++ interp.ml
@@ -8,45 +8,33 @@ let (+:) (xa, ya) (xb, yb) =
 let int_pos (x, y) =
   (int_of_float x, int_of_float y)
 
-let bullet_spr = ref None
-
-let bullet_sprite () :Graphics.image =
-  let mk () =
-    let o = Graphics.white in
-    let x = Graphics.black in
-    Graphics.make_image
-      [| [|o;o;o;x;x;o;o;o|]
-       ; [|o;o;x;x;x;x;o;o|]
-       ; [|o;x;x;x;x;x;x;o|]
-       ; [|x;x;x;x;x;x;x;x|]
-       ; [|x;x;x;x;x;x;x;x|]
-       ; [|o;x;x;x;x;x;x;o|]
-       ; [|o;o;x;x;x;x;o;o|]
-       ; [|o;o;o;x;x;o;o;o|]
-      |]
-  in
-  match !bullet_spr with
-  | Some s -> s
-  | None -> begin
-      let s = mk () in
-      bullet_spr := Some s;
-      s
-    end
-
-let draw_bullet p =
-  let (x, y) = int_pos (p +: (-4., -4.)) in
-  Printf.printf "(%d, %d)\n" x y;
-  Graphics.draw_image (bullet_sprite ()) x y
-
-let _ =
-  Graphics.open_graph " 200x200";
-  let center = (100., 100.) in
+let draw_frame (window:OcsfmlGraphics.render_window) =
+  let open OcsfmlGraphics in
+  let background = new rectangle_shape () in
+  window#draw background;
   for i = 0 to 36 do
     let t = from_deg (10.0 *. float i) in
     let sz = 50.0 in
     let delta = (sz *. cos t, sz *. sin t) in
-    let pos = center +: delta in
-    draw_bullet pos
-  done;
-  let _ = read_line () in
-  Graphics.close_graph ()
+    let center = (100., 100.) in
+    let position = center +: delta in
+    let texture = new texture  (`File "bullet.png") in
+    let sprite = new sprite ~texture ~position () in
+    window#draw sprite;
+  done
+
+let _ =
+  let open OcsfmlWindow in
+  let open OcsfmlGraphics in
+  let mode = VideoMode.create ~w:200 ~h:200 () in
+  let window = new render_window mode "BulletML" in
+  let rec main_loop () =
+    if window#is_open
+    then begin
+      window#clear ();
+      draw_frame window;
+      window#display;
+      main_loop ()
+    end
+  in
+  main_loop ()
