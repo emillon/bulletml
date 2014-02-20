a64672d4835d9d99f827ea8721023eb41eccfc3c
Etienne Millon <me@emillon.org>
2014-02-20 19:35:07 +0100

    limit framerate

--- interp.ml
+++ interp.ml
@@ -8,33 +8,33 @@ let (+:) (xa, ya) (xb, yb) =
 let int_pos (x, y) =
   (int_of_float x, int_of_float y)
 
-let draw_frame (window:OcsfmlGraphics.render_window) =
+let draw_frame (window:OcsfmlGraphics.render_window) i =
   let open OcsfmlGraphics in
   let background = new rectangle_shape () in
   window#draw background;
-  for i = 0 to 36 do
-    let t = from_deg (10.0 *. float i) in
-    let sz = 50.0 in
-    let delta = (sz *. cos t, sz *. sin t) in
-    let center = (100., 100.) in
-    let position = center +: delta in
-    let texture = new texture  (`File "bullet.png") in
-    let sprite = new sprite ~texture ~position () in
-    window#draw sprite;
-  done
+  let t = from_deg (10.0 *. float i) in
+  let sz = 50.0 in
+  let delta = (sz *. cos t, sz *. sin t) in
+  let center = (100., 100.) in
+  let position = center +: delta in
+  let texture = new texture  (`File "bullet.png") in
+  let sprite = new sprite ~texture ~position () in
+  window#draw sprite
 
 let _ =
   let open OcsfmlWindow in
   let open OcsfmlGraphics in
   let mode = VideoMode.create ~w:200 ~h:200 () in
   let window = new render_window mode "BulletML" in
-  let rec main_loop () =
+  window#set_framerate_limit 60;
+  window#set_vertical_sync_enabled true;
+  let nframe = ref 0 in
+  while true; do
     if window#is_open
     then begin
       window#clear ();
-      draw_frame window;
+      draw_frame window (!nframe);
       window#display;
-      main_loop ()
-    end
-  in
-  main_loop ()
+    end;
+    incr nframe
+  done
