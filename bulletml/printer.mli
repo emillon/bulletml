f40334df0a5ec09e7dfac58e6752b10079838085
Etienne Millon <me@emillon.org>
2014-02-27 23:41:12 +0100

    Document printer

---
+++ bulletml/printer.mli
@@ -0,0 +1,15 @@
+(** Print syntactic values. *)
+
+type 'a printer = 'a -> string
+
+(** Print a list.
+
+    Given a way to print an ['a], print a list of ['a].
+*)
+val print_list : 'a printer -> 'a list printer
+
+(** Print a {!Syntax.t}. *)
+val print_bulletml : Syntax.t printer
+
+(** Print an {!Interp_types.opcode}. *)
+val print_opcode : Interp_types.opcode printer
