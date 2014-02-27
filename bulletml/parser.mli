a5a2e05c7d835b9b97ab2ef2b4a35d74820c6dcc
Etienne Millon <me@emillon.org>
2014-02-27 23:26:37 +0100

    document parser

---
+++ bulletml/parser.mli
@@ -0,0 +1,22 @@
+(** Convert a XML document to a {!Syntax.t}. *)
+
+(** Convert a whole document to a whole {!Syntax.t}. *)
+val parse_xml : Xml.xml -> Syntax.t
+
+(** Parse only a {!Syntax.action}.
+
+    The argument is a list of children of an [<action>] element.
+*)
+val parse_action : Xml.xml list -> Syntax.action
+
+(** Parse only a {!Syntax.bullet}.
+
+    The argument is a list of children of a [<bullet>] element.
+*)
+val parse_bullet : Xml.xml list -> Syntax.bullet
+
+(** Parse only a {!Syntax.fire}.
+
+    The argument is a list of children of a [<fire>] element.
+*)
+val parse_fire : Xml.xml list -> Syntax.fire
