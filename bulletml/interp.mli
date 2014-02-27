8f15e7f38bb1cdfe9bdc69a6903a0dfee6892771
Etienne Millon <me@emillon.org>
2014-02-27 21:47:51 +0100

    document interp.mli

--- bulletml/interp.mli
+++ bulletml/interp.mli
@@ -1,21 +1,54 @@
 open Syntax
 open Interp_types
 
+(** Functions to build a BulletML interpreter. *)
+
+(** 
+   Convert a list of {!Syntax.subaction}s into a list of {!Interp_types.opcode}s.
+   The extra list given as argument is the list to prepend to.
+*)
 val build_prog : env -> opcode list -> subaction -> opcode list
 
+(**
+   Convert a (float) {!Interp_types.position} to a pair of ints.
+*)
 val int_pos : position -> (int * int)
 
+(**
+   Given a program and a position, build an {!Interp_types.obj} with default
+   values.
+*)
 val initial_obj : opcode list -> position -> obj
 
+(**
+   Create the list of descendants of an {!Interp_types.obj}, including itself.
+*)
 val collect_obj : obj -> obj list
 
+(**
+   Compute the next state of an {!Interp_types.obj}:
+     - delete out-of-bounds or vanished children
+     - recursively {!animate} its remaining children
+     - interpret its current instruction
+     - handle physics (move it according to its current direction and speed)
+
+   The interpretation phase can interpret several {!Interp_types.opcode}s, for
+   example until a [OpWait] instruction is found.
+*)
 val animate : env -> obj -> obj
 
+(**
+   Compile and prepare a program for interpretation.
+   @return an environement, an initial object and the name of the top pattern
+*)
 val prepare
   :  Syntax.t
   -> init_params
-  -> env * obj * string (** env, root object, name of top pattern *)
+  -> env * obj * string
 
+(**
+   Skeleton for a main loop. See {!Interp_types.interpreter}.
+*)
 val main_loop
   :  Syntax.t
   -> init_params
