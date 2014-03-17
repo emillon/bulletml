0ba4c6cbfc927a5f15e84ff10ac9faab5378872c
Etienne Millon <me@emillon.org>
2014-03-17 15:42:02 +0100

    draw ship in sdl app

--- run.ml
+++ run.ml
@@ -13,9 +13,15 @@ let make_global_ctx () =
   Sdl.init ~auto_clean:true [`VIDEO;`NOPARACHUTE];
   let surf = Sdlvideo.set_video_mode ~w:screen_w ~h:screen_h [] in
   let bullet = Sdlloader.load_image "bullet.png" in
-  (surf, bullet)
+  let ship =
+    Sdlvideo.create_RGB_surface []
+      ~w:8 ~h:8 ~bpp:32
+      ~rmask:0l ~gmask:0l ~bmask:0l ~amask:0l
+  in
+  Sdlvideo.fill_rect ship 0x69D2E7l;
+  (surf, bullet, ship)
 
-let make_local_ctx (surf, bullet) =
+let make_local_ctx ctx =
   Sdlevent.pump ();
   begin
     match Sdlevent.poll () with
@@ -23,17 +29,17 @@ let make_local_ctx (surf, bullet) =
            | Sdlevent.KEYDOWN { Sdlevent.keysym = Sdlkey.KEY_q } ) -> raise Exit
     | _ -> ()
   end;
-  (surf, bullet)
+  ctx
 
-let clear (surf, _) =
+let clear (surf, _, _) =
   let rect = Sdlvideo.rect ~x:0 ~y:0 ~h:screen_h ~w:screen_w in
   Sdlvideo.fill_rect ~rect surf 0x00ffffffl
 
-let run_cont (surf, _) k =
+let run_cont (surf, _, _) k =
   Sdlvideo.flip surf;
   k ()
 
-let draw_bullet (window, bullet) b =
+let draw_bullet (window, bullet, _) b =
   let (px, py) = int_pos b.pos in
   let dst_rect = Sdlvideo.rect ~x:px ~y:py ~w:0 ~h:0 in
   Sdlvideo.blit_surface ~src:bullet ~dst:window ~dst_rect ()
@@ -45,6 +51,11 @@ let draw ctx root =
       (collect_obj root)
   in
   List.iter (draw_bullet ctx) objs
+
+let draw_ship (window, bullet, ship) pos =
+  let (px, py) = int_pos pos in
+  let dst_rect = Sdlvideo.rect ~x:px ~y:py ~w:0 ~h:0 in
+  Sdlvideo.blit_surface ~src:ship ~dst:window ~dst_rect ()
 
 let main () =
   let (fname, patname) = match Sys.argv with
@@ -65,7 +76,7 @@ let main () =
     ; make_local_ctx
     ; clear
     ; draw
-    ; draw_ship = (fun _ _ -> ())
+    ; draw_ship
     ; run_cont
     }
 
