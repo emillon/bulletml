f0ee1278b44b06ad5ed1288dc5ecc6ad78fb112b
Etienne Millon <me@emillon.org>
2014-03-17 15:59:16 +0100

    refactor run to use a record as ctx

--- run.ml
+++ run.ml
@@ -9,9 +9,15 @@ let enemy_pos = (float screen_w /. 2., float screen_h *. 0.3)
 
 let ship_pos = (float screen_w /. 2., float screen_h *.0.9)
 
+type global_ctx =
+  { window : Sdlvideo.surface
+  ; bullet : Sdlvideo.surface
+  ; ship : Sdlvideo.surface
+  }
+
 let make_global_ctx () =
   Sdl.init ~auto_clean:true [`VIDEO;`NOPARACHUTE];
-  let surf = Sdlvideo.set_video_mode ~w:screen_w ~h:screen_h [] in
+  let window = Sdlvideo.set_video_mode ~w:screen_w ~h:screen_h [] in
   let bullet = Sdlloader.load_image "bullet.png" in
   let ship =
     Sdlvideo.create_RGB_surface []
@@ -19,7 +25,7 @@ let make_global_ctx () =
       ~rmask:0l ~gmask:0l ~bmask:0l ~amask:0l
   in
   Sdlvideo.fill_rect ship 0x69D2E7l;
-  (surf, bullet, ship)
+  { window ; bullet; ship }
 
 let make_local_ctx ctx =
   Sdlevent.pump ();
@@ -31,15 +37,15 @@ let make_local_ctx ctx =
   end;
   ctx
 
-let clear (surf, _, _) =
+let clear { window } =
   let rect = Sdlvideo.rect ~x:0 ~y:0 ~h:screen_h ~w:screen_w in
-  Sdlvideo.fill_rect ~rect surf 0x00ffffffl
+  Sdlvideo.fill_rect ~rect window 0x00ffffffl
 
-let run_cont (surf, _, _) k =
-  Sdlvideo.flip surf;
+let run_cont { window } k =
+  Sdlvideo.flip window;
   k ()
 
-let draw_bullet (window, bullet, _) b =
+let draw_bullet {window;bullet} b =
   let (px, py) = int_pos b.pos in
   let dst_rect = Sdlvideo.rect ~x:px ~y:py ~w:0 ~h:0 in
   Sdlvideo.blit_surface ~src:bullet ~dst:window ~dst_rect ()
@@ -52,10 +58,20 @@ let draw ctx root =
   in
   List.iter (draw_bullet ctx) objs
 
-let draw_ship (window, bullet, ship) pos =
+let draw_ship { window ; bullet ; ship } pos =
   let (px, py) = int_pos pos in
   let dst_rect = Sdlvideo.rect ~x:px ~y:py ~w:0 ~h:0 in
   Sdlvideo.blit_surface ~src:ship ~dst:window ~dst_rect ()
+
+let interp :(global_ctx, global_ctx, unit) interpreter =
+  { make_global_ctx
+  ; make_local_ctx
+  ; clear
+  ; draw
+  ; draw_ship
+  ; move_ship = (fun _ p -> p)
+  ; run_cont
+  }
 
 let main () =
   let (fname, patname) = match Sys.argv with
@@ -72,13 +88,6 @@ let main () =
     ; p_screen_w = screen_w
     ; p_screen_h = screen_h
     }
-    { make_global_ctx
-    ; make_local_ctx
-    ; clear
-    ; draw
-    ; draw_ship
-    ; move_ship = (fun _ p -> p)
-    ; run_cont
-    }
+    interp
 
 let _ = main ()
