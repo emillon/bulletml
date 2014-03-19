a04bbac7231443ef2fb1745a71b65c3a0b9b3c3a
Etienne Millon <me@emillon.org>
2014-03-19 17:04:12 +0100

    new pattern in JS demo

--- jsapp.ml
+++ jsapp.ml
@@ -2,65 +2,16 @@ open Bulletml.Syntax
 open Bulletml.Interp
 open Bulletml.Interp_types
 
-let bml = (* {{{ *)
-  BulletML (NoDir,
-            [ EBullet ("fast",
-                       Bullet (None, Some (SpdAbs (Num 10.)), [ Direct (
-                           [ Wait (Num 6.)
-                           ; ChangeSpeed (SpdAbs (Num 0.), Num 1.)
-                           ; Wait (Num 20.)
-                           ; Repeat ((Num 10. +@ Rank *@ Num 18.), Direct (
-                               [ Fire (Direct (( Some (DirSeq (((~@ (Num 11.)) -@ Rand *@ Num 2.)))
-                                               , Some (SpdAbs (Num 1.5))
-                                               , Direct bulletDefault)))
-                               ; Action (Indirect ("add3", []))
-                               ; Repeat (Num 4., Direct (
-                                   [ Fire (Direct (( Some (DirSeq (Num 0.))
-                                                   , Some (SpdSeq ((Num 0.1 +@ (Rank *@ Num 0.2))))
-                                                   , Direct bulletDefault)))
-                                   ; Action (Indirect ("add3", []))
-                                   ]))
-                               ; Wait ((Num 336. /@ (Num 10. +@ Rank *@ Num 18.)))]))
-                           ; Vanish
-                           ])]))
-            ; EAction ("add3",
-                       [ Repeat (Num 3., Direct ([Fire (Direct (( Some (DirSeq (Num 90.))
-                                                                , Some (SpdSeq (Num 0.))
-                                                                , Direct bulletDefault)))]))
-                       ])
-            ; EFire ("slowColorChange",
-                     ( Some (DirAbs ((Num 180. +@ Num 45. *@ Param 1)))
-                     , Some (SpdAbs (Num 7.))
-                     , Direct (Bullet (None, None, [Direct (
-                         [ Wait (Num 6.)
-                         ; ChangeSpeed (SpdAbs (Num 0.), Num 1.)
-                         ; Repeat ((Num 50. +@ Rank *@ Num 50.), Direct (
-                             [ Fire (Direct (( Some (DirSeq (((Num 8. -@ Rank *@ Num 4.) *@ Param 1)))
-                                             , Some (SpdAbs (Num 1.2))
-                                             , Direct bulletDefault)))
-                             ; Action (Indirect ("add3", []))
-                             ; Wait (((Num 8. -@ Rank *@ Num 4.) +@ Rand))
-                             ]))
-                         ; Vanish
-                         ])]))))
-            ; EFire ("slow",
-                     ( None
-                     , None
-                     , Direct (Bullet (None, None, [Direct (
-                         [ Fire (Indirect ("slowColorChange", [Param 1]))
-                         ; Vanish
-                         ])]))))
-            ; EAction ("top",
-                       [ Fire (Direct ((Some (DirAbs (~@ (Num 85.))), None, Indirect ("fast", []))))
-                       ; Wait (Num 1.)
-                       ; Fire (Direct ((Some (DirAbs (Num 85.)), None, Indirect ("fast", []))))
-                       ; Wait (Num 1.)
-                       ; Fire (Indirect ("slow", [Num (1.)]))
-                       ; Wait (Num 1.)
-                       ; Fire (Indirect ("slow", [~@ (Num 1.)]))
-                       ; Wait (Num 430.)
-                       ])
-            ]) (* }}} *)
+let bml = (*{{{*)
+BulletML (Horizontal, [EAction ("top",[Repeat (Num 20.,Direct ([Fire (Direct
+((Some (DirAim (((Num 0. -@ Num 60.) +@ (Rand *@ Num 120.)))), None, Indirect
+("hmgLsr", [])))); Repeat (Num 8.,Direct ([Wait (Num 1.); Fire (Direct ((Some
+(DirSeq (Num 0.)), None, Indirect ("hmgLsr", []))))])); Wait (Num 10.)])); Wait
+(Num 60.)]); EBullet ("hmgLsr",Bullet (None,Some (SpdAbs (Num 2.)),[Direct
+([ChangeSpeed (SpdAbs (Num 0.3),Num 30.); Wait (Num 100.); ChangeSpeed (SpdAbs
+(Num 5.),Num 100.)]); Direct ([Repeat (Num 12.,Direct ([ChangeDirection (DirAim
+(Num 0.),(Num 45. -@ (Rank *@ Num 30.))); Wait (Num 5.)]))])]))])
+  (*}}}*)
 
 let screen_w = 400
 let screen_h = 300
