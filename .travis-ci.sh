ed8683d9446a27210634128784de54427da6e2ef
Etienne Millon <me@emillon.org>
2015-05-25 15:18:40 +0200

    don't download patched bisect

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -31,20 +31,6 @@ opam install ocp-indent.1.5
 ocp-indent -i *.ml bulletml/*.ml bulletml/*.mli tests/*.ml
 [ -z "$(git diff)" ]
 
-echo "0092a42114e7937ad06d1f19b6345c41a84196ad  bisect-coveralls.tar.gz" > bisect.SHA1SUMS
-wget \
-    http://sagotch.fr/download/bisect-coveralls.tar.gz \
-    https://raw.github.com/sagotch/ocveralls/9069356076e886ad0913fbc8550330f45d3cc664/ocveralls.sh
-sha1sum -c bisect.SHA1SUMS
-tar -xvf bisect-coveralls.tar.gz
-cd Bisect
-chmod +x configure
-./configure
-cat Makefile.config
-make all
-sudo make install
-cd ..
-
 ocp-build -init bulletml
 make check
 make js
