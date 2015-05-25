5797b953e64970a517f63ba27768d503073a9c31
Etienne Millon <me@emillon.org>
2015-05-25 14:42:53 +0200

    upgrade OPAM

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -6,6 +6,7 @@ case "$OCAML_VERSION,$OPAM_VERSION" in
 4.00.1,1.1.0) ppa=avsm/ocaml40+opam11 ;;
 4.01.0,1.0.0) ppa=avsm/ocaml41+opam10 ;;
 4.01.0,1.1.0) ppa=avsm/ocaml41+opam11 ;;
+4.01.0,1.2.1) ppa=avsm/ocaml41+opam12 ;;
 *) echo Unknown $OCAML_VERSION,$OPAM_VERSION; exit 1 ;;
 esac
 
