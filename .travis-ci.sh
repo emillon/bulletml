6027aa97da8115f7529a8514fae7109765725cca
Etienne Millon <me@emillon.org>
2016-06-22 23:14:03 +0200

    build the js app, maybe

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,5 +1,21 @@
 bash -ex .travis-opam.sh
 eval $(opam config env)
-make js
+ocaml_version=$(opam config var ocaml-version)
+
+function build_js () {
+    opam install js_of_ocaml
+    make js
+}
+
+case $ocaml_version in
+    4.02.2)
+        build_js
+        ;;
+    *)
+        echo "Unknown ocaml version: $ocaml_version"
+        exit 1
+        ;;
+esac
+
 make doc
 bash -ex .ci-indent.sh
